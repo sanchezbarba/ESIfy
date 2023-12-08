6f95cc001d14ffc8037acc17605c1edf5703e01a
alru2002 <115723072+alru2002@users.noreply.github.com>
2023-12-08 20:05:18 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
 // Obtener el valor del parámetro 'gif'
 var gifValue = urlParams.get('gif');
-var songValue = urlParams.get('cancion')
+var songValue = urlParams.get('cancion');
+var posicion = urlParams.get('posicion');
+
 // Utilizar el valor del parámetro 'gif'
 const gif = document.getElementById('GIF');
 const audio = document.getElementById('audioPlayer');
@@ -12,7 +14,6 @@ const seekBar = document.getElementById('seekBar');
 const Descarga = document.getElementById('Descarga');
 const volumeSlider = document.getElementById('volumeSlider');
 const iconoVolumen = document.querySelector('.icono-volumen');
-
 ultimoVolumen = 0;
 // Reproduccion 
 Descarga.setAttribute('href',songValue)
@@ -20,6 +21,27 @@ console.log(Descarga.getAttribute('href'))
 gif.setAttribute('src',gifValue)
 audio.setAttribute('src',songValue)
 let isPlaying = false;
+const Canciones = [
+    ["../img/GIFS/Schubert.gif","../Canciones/Schubert.mp3","Schuber Stabat Mater in F Minor"],
+    ["../img/GIFS/Chopin.gif","../Canciones/Chopin.mp3","Etude in C Mayor, Op 10 No:1"],
+    ["../img/GIFS/Concertino.gif","../Canciones/Concertino.mp3","Guitar Concertion in A Minor"],
+    ["../img/GIFS/Columbia.gif","cancion=../Canciones/Columbia.mp3","Columbia"],
+    ["../img/GIFS/Primavera.gif","../Canciones/Primavera.mp3","La primavera"],
+    ["../img/GIFS/Shakira.gif","../Canciones/Shakira.mp3","Shakira bzrp music sessions"],
+    ["../img/GIFS/Supernova.gif","../Canciones/Supernova.mp3","Supernova"],
+    ["../img/GIFS/Starboy.gif","../Canciones/Starboy.mp3","Starboy"],
+    ["../img/GIFS/Strangers.gif","../Canciones/Strangers.mp3","Strangers"],
+    ["../img/GIFS/GodPlans.gif","../Canciones/GodPlans.mp3","God Plans"],
+    ["../img/GIFS/TodoDeTi.gif","../Canciones/TodoDeTi.mp3","Todo de Ti"],
+    ["../img/GIFS/Meduza.gif","../Canciones/Meduza.mp3","Piece Of Your Heart"]
+
+];
+
+
+var songTitleElement = document.getElementById('songTitle');
+songTitleElement.innerText = Canciones[posicion][2];
+
+
 
 
 iconoVolumen.addEventListener('click', function () {
@@ -87,6 +109,7 @@ function initializePlayer() {
     var durationMinutes = Math.floor(audio.duration / 60);
     var durationSeconds = Math.floor(audio.duration % 60);
     durationElement.innerText = durationMinutes + ":" + (durationSeconds < 10 ? "0" : "") + durationSeconds;
+    
 }
 
 var currentTimeElement = document.getElementById("currentTime");
@@ -110,3 +133,28 @@ function cambiarIcono() {
         iconoVolumen.innerHTML = '<i class="fa-solid fa-volume-high"></i>';
     }
 }
+const Nextbutton = document.getElementById('NextButton');
+
+Nextbutton.addEventListener('click',function(){
+    const icon = playPauseButton.querySelector('i');
+    if (posicion < Canciones.length - 1) {
+        // Incrementar la posición para reproducir la siguiente canción
+        posicion++;
+        seekBar.value = 0;
+        console.log(seekBar.value);
+        // Establecer los atributos src con la información de la próxima canción
+        gif.setAttribute('src', Canciones[posicion][0]);
+        audio.setAttribute('src', Canciones[posicion][1]);
+        // Reproducir automáticamente la siguiente canción
+        
+        audio.duration = 0;
+        isPlaying = false;
+        icon.classList.remove('fa-pause');
+		icon.classList.add('fa-play');
+        songTitleElement.innerText = Canciones[posicion][2];
+
+        // Actualizar la barra de reproducción
+        
+    }
+});
+
